d5b016d1941dfb356646fcbb3ce22b2f267e1637
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-08-31 15:02:54 -0300

    start the first screen

--- mobile/app/src/main/java/com/example/flashcards_app/MainActivity.java
+++ mobile/app/src/main/java/com/example/flashcards_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.flashcards_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
