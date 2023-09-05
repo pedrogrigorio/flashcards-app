ee3b1be555b9199f289b084b85b8188fb1795636
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-09-05 10:07:13 -0300

    create and edit deck popups

--- mobile/app/src/main/java/com/example/flashcards_app/MainActivity.java
+++ mobile/app/src/main/java/com/example/flashcards_app/MainActivity.java
@@ -1,28 +1,90 @@
 package com.example.flashcards_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout parent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        parent = findViewById(R.id.parent_layout);
+
+        Button createDeck = findViewById(R.id.btn_create_deck);
+        createDeck.setOnClickListener(v -> {
+            showCreateDeckPopupWindow();
+        });
+
+        Button editDeck = findViewById(R.id.btn_edit_deck);
+        editDeck.setOnClickListener(v -> {
+            showEditDeckPopupWindow();
+        });
+
     }
 
-    public void acessPerfil(View view) {
+    public void accessProfile(View v) {
         Intent in = new Intent(MainActivity.this, ProfileActivity.class);
         startActivity(in);
     }
 
-    public void acessNotifications(View view) {
+    public void accessNotifications(View v) {
         Intent in = new Intent(MainActivity.this, NotificationActivity.class);
         startActivity(in);
+    }
+
+    public void showCreateDeckPopupWindow() {
+        View view = View.inflate(this, R.layout.create_deck_popup, null);
+        ImageView close = view.findViewById(R.id.create_deck_close_popup);
+        TextView cancel = view.findViewById(R.id.cancel_create_deck);
+
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+
+        PopupWindow popupWindow = new PopupWindow(view, width, height, false);
+
+        popupWindow.showAtLocation(parent, Gravity.CENTER, 0, 0);
+
+        close.setOnClickListener(v -> {
+            popupWindow.dismiss();
+        });
+
+        cancel.setOnClickListener(v -> {
+            popupWindow.dismiss();
+        });
+    }
+
+    public void showEditDeckPopupWindow() {
+        View view = View.inflate(this, R.layout.edit_deck_popup, null);
+        ImageView close = view.findViewById(R.id.edit_deck_close_popup);
+        TextView cancel = view.findViewById(R.id.cancel_edit_deck);
+
+        int width = ViewGroup.LayoutParams.MATCH_PARENT;
+        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+
+        PopupWindow popupWindow = new PopupWindow(view, width, height, false);
+
+        popupWindow.showAtLocation(parent, Gravity.CENTER, 0, 0);
+
+        close.setOnClickListener(v -> {
+            popupWindow.dismiss();
+        });
+
+        cancel.setOnClickListener(v -> {
+            popupWindow.dismiss();
+        });
     }
 }
