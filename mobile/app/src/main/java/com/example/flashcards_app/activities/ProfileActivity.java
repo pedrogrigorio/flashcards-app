c8c10adaab066487940bad33cb19c5d8dcc94b66
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-09-17 12:38:18 -0300

    profile image picker

--- mobile/app/src/main/java/com/example/flashcards_app/activities/ProfileActivity.java
+++ mobile/app/src/main/java/com/example/flashcards_app/activities/ProfileActivity.java
@@ -1,22 +1,48 @@
 package com.example.flashcards_app.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.example.flashcards_app.R;
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView profilePhoto;
+    FloatingActionButton camButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profilePhoto = findViewById(R.id.profile_img);
+        camButton = findViewById(R.id.btn_cam);
+
+        camButton.setOnClickListener(v -> {
+            ImagePicker.with(this)
+                    .crop(1f, 1f)
+                    .compress(1024)
+                    .maxResultSize(1080, 1080)
+                    .start();
+        });
     }
 
-    public void acessMainScreen(View view) {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        profilePhoto.setImageURI(uri);
+    }
+
+    public void accessMainScreen(View view) {
         Intent in = new Intent(ProfileActivity.this, MainActivity.class);
         startActivity(in);
     }
