4d3769ed84b42f6c4efb39d09ec82d2fe95b631d
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-10-19 09:26:16 -0300

    implement User class

--- mobile/app/src/main/java/com/example/flashcards_app/models/User.java
+++ mobile/app/src/main/java/com/example/flashcards_app/models/User.java
@@ -2,11 +2,14 @@ package com.example.flashcards_app.models;
 
 public class User {
 
-    int id;
-
     String name;
-    public User(int id) {
-        this.id = id;
+    String username;
+    String imgSrc;
+
+    public User(String name, String username) {
+        this.name = name;
+        this.username = username;
+        this.imgSrc = "https://cdn.vectorstock.com/i/preview-1x/08/19/gray-photo-placeholder-icon-design-ui-vector-35850819.jpg";
     }
 
     public String getName() {
@@ -15,5 +18,21 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImgSrc() {
+        return imgSrc;
+    }
+
+    public void setImgSrc(String imgSrc) {
+        this.imgSrc = imgSrc;
     }
 }
