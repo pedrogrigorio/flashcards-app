d7ba3a2e0fd0a386dfb9c5b531d024bbdc2d8a0a
RichardeLira <richarde-ips@outlook.com>
2023-10-22 13:16:49 -0300

    Add user friend activity

--- mobile/app/src/main/java/com/example/flashcards_app/activities/AddingFriendsActivity.java
+++ mobile/app/src/main/java/com/example/flashcards_app/activities/AddingFriendsActivity.java
@@ -3,13 +3,38 @@ package com.example.flashcards_app.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
-
+import com.example.flashcards_app.R;
+import com.example.flashcards_app.adapters.AddFriendAdapter;
+import com.example.flashcards_app.viewmodel.AddFriendViewModel;
 
 public class AddingFriendsActivity extends AppCompatActivity {
+
+    private AddFriendViewModel addFriendViewModel;
+    private RecyclerView recyclerView;
+    private AddFriendAdapter adapter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_user_friends);
+
+        adapter = new AddFriendAdapter();
+
+
+
     }
+
+
+    private void ConfigRecyclerView() {
+
+    }
+
 }
+
+
+
