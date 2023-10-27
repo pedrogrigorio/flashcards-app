24e299799eed49f3ec0f65c9d4bb363cede7728a
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-10-27 20:37:21 -0300

    rename service class for add user

--- mobile/app/src/main/java/com/example/flashcards_app/repositories/AddUserRepository.java
+++ mobile/app/src/main/java/com/example/flashcards_app/repositories/AddUserRepository.java
@@ -2,7 +2,7 @@ package com.example.flashcards_app.repositories;
 
 import androidx.lifecycle.MutableLiveData;
 
-import com.example.flashcards_app.api.AddingUserService;
+import com.example.flashcards_app.api.AddUserService;
 import com.example.flashcards_app.models.User;
 import com.example.flashcards_app.util.RetrofitClient;
 
@@ -14,17 +14,17 @@ import retrofit2.Response;
 
 public class AddUserRepository {
 
-    private AddingUserService addingUserService;
+    private AddUserService addUserService;
 
 
     public AddUserRepository() {
-        addingUserService = RetrofitClient.getRetrofitInstance().create(AddingUserService.class);
+        addUserService = RetrofitClient.getRetrofitInstance().create(AddUserService.class);
     }
 
     public MutableLiveData<List<User>> getNewFriendsToAdd() {
         MutableLiveData<List<User>> friendsAddLiveData = new MutableLiveData<>();
 
-        Call<List<User>> call = addingUserService.getAllFriends();
+        Call<List<User>> call = addUserService.getAllFriends();
         call.enqueue(new Callback<List<User>>() {
             @Override
             public void onResponse(Call<List<User>> call, Response<List<User>> response) {
