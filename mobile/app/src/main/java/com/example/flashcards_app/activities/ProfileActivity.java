08c4d16771451573d8d8b6e6bc99993e9ce652a2
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-10-29 23:49:56 -0300

    edit name popup

--- mobile/app/src/main/java/com/example/flashcards_app/activities/ProfileActivity.java
+++ mobile/app/src/main/java/com/example/flashcards_app/activities/ProfileActivity.java
@@ -9,16 +9,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.flashcards_app.R;
+import com.example.flashcards_app.dialogs.EditDeckDialog;
+import com.example.flashcards_app.dialogs.EditNameDialog;
+import com.example.flashcards_app.models.Deck;
+import com.example.flashcards_app.models.User;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    User user;
     ImageView profilePhoto;
     FloatingActionButton camButton;
+    ImageView editNameButton;
+    TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +35,24 @@ public class ProfileActivity extends AppCompatActivity {
 
         profilePhoto = findViewById(R.id.profile_img);
         camButton = findViewById(R.id.btn_cam);
+        editNameButton = findViewById(R.id.btn_edit_name);
+        name = findViewById(R.id.name);
+
+        user = new User("User23", "user3");
+        name.setText(user.getName());
+
+        editNameButton.setOnClickListener(v -> {
+            EditNameDialog dialog = new EditNameDialog(user);
+            dialog.setDialogResult(new EditNameDialog.onDialogResult() {
+                @Override
+                public void finish(User updatedProfile) {
+                    user = updatedProfile;
+                    name.setText(user.getName());
+//                    profileViewModel.updateProfile(updatedProfile);
+                }
+            });
+            dialog.show(getSupportFragmentManager(), "edit_name_popup");
+        });
 
         camButton.setOnClickListener(v -> {
             ImagePicker.with(this)
