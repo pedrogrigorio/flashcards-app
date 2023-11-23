6760eb61fe491ed36a1730a99d49082b034d698c
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-11-23 17:09:53 -0300

    delete hello endpoint

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express'
-import helloRoutes from './hello'
 import userRoutes from './user'
 import authRoutes from './auth'
 
 const routes = Router()
-
-routes.use(helloRoutes)
 routes.use(userRoutes)
 routes.use(authRoutes)
 
